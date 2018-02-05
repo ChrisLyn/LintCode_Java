1a38ba466685f1b02b86eaa98e891f1759e823cd
ChrisLyn <saffercsmith@gmail.com>
2018-02-04 20:27:42 -0700

    463 sort integer

---
+++ SortInteger.java
@@ -0,0 +1,44 @@
+public class SortInteger {
+    /*
+     * @param A: an integer array
+     * @return: 
+     */
+    public void sortIntegers(int[] A) {
+        // write your code here
+        if (A == null || A.length == 0) {
+            return;
+        }
+        
+        quickSort(A, 0, A.length - 1);
+    }
+    
+    private void quickSort(int[] A, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        
+        int left = start, right = end;
+        // 1. pivot, No A[start], No A[end]
+        // get value not index
+        int pivot = A[start + (end - start) / 2];
+        // pivot = A[start] // pivot = A[end];
+        // left <= right not left < right
+        while (left  <= right) {
+            while (left <= right && A[left] < pivot) {
+                left++;
+            }
+            while (left <= right && A[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        quickSort(A, start, right);
+        quickSort(A, left, end);
+    }
+}
