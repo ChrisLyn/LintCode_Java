34450f31fe1d765c885c85593772313ef18212d6
ChrisLyn <saffercsmith@gmail.com>
2018-02-11 00:39:17 -0700

    31 Partition Array

---
+++ PartitionArray.java
@@ -0,0 +1,33 @@
+public class PartitionArray {
+    /*
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] < k) {
+                swap(nums, i, left);
+                left++;
+                i++;
+            } else {
+                swap(nums, i, right);
+                right--;
+            }
+        }
+        return i;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
